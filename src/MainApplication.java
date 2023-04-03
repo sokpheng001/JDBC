7b18a26dc768e3a217115c1dd941f7f18fbc0d4f
sokpheng001 <115383587+sopi001@users.noreply.github.com>
2023-04-03 23:40:24 +0700

    JDBC

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -6,7 +6,7 @@ import view.View;
 import java.sql.*;
 import java.util.*;
 
-public class Main {
+public class MainApplication {
     private static final Set<Istad_Teacher> list = new HashSet<>();
     private static Datasource datasource = new Datasource();
     public static void main(String[] args) throws SQLException {
